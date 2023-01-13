d418d5499822dc81a582d18743325c7506baa6d4
NehaQA <neha.arora@crownstack.com>
2023-01-13 13:24:25 +0530

    new

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,7 @@ describe('login Test Cases', () => {
     
   it('Verify user is able to login successfully with valid credentials', () => {
       cy.visit("https://app.keka.com/Account/Login");
-      
+      cy.visit("");
 
 
   });
