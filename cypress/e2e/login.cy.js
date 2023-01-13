0291023d4502b22b77b0377b635d3eaa46108759
NehaQA <neha.arora@crownstack.com>
2023-01-13 16:37:03 +0530

    new test cases added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,22 @@
 
 describe('login Test Cases', () => {
     
-  it('Verify user is able to login successfully with valid credentials', () => {
+  it('Verify user is able to redirect to Keka Website', () => {
       cy.visit("https://app.keka.com/Account/Login");
     //  cy.visit("");
+        
+    it('Verify that user should be able to login with valid username and password',function()
+    {
+        cy.get('#email').type('neha.arora@crownstack.com')
+        cy.get('#password').type('Password@123')
+        cy.get('#Login').click()
+        cy.wait(500)
+        cy.contains("Crownstack Technologies Pvt Ltd").should('be.visible')
+    })
+    
 
 
   });
+
+
 });
