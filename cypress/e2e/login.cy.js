a8b385b58f7809ff3e35281b41c8ce802cc94725
NehaQA <neha.arora@crownstack.com>
2023-01-13 13:22:07 +0530

    first commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,10 @@
+
+describe('login Test Cases', () => {
+    
+  it('Verify user is able to login successfully with valid credentials', () => {
+      cy.visit("https://app.keka.com/Account/Login");
+      
+
+
+  });
+});
